404fe95f60967002e4a5c90f173ed120bbe47427
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-10-31 21:40:25 +0700

    Add Customer Resource, Modify Table and Form

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@admin.com'
+        ]);
+
+        Customer::factory()
+            ->count(10)
+            ->create();
     }
 }
