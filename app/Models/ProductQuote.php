9510b1df7dbfcd7bff28e233464deb3124dfe655
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-11-08 21:21:03 +0700

    tambah quoutes dengan product

---
+++ app/Models/ProductQuote.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductQuote extends Model
+{
+    public $incrementing = true;
+    public $timestamps = false;
+    
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn($value) => $value * 100,
+        );
+    }
+
+    public function quote(): BelongsTo
+    {
+        return $this->belongsTo(Quote::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
